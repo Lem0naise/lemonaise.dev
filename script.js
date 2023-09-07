c2cd98fab6396f7798814cf1dad239abf1ebde4b
Zac Nolan <zac1i1nolan@gmail.com>
2023-09-07 14:23:57 +0100

    Circle

---
+++ script.js
@@ -0,0 +1,11 @@
+//DOM load event
+window.addEventListener("DOMContentLoaded", () => {
+    const spotlight = document.querySelector('#spotlight');
+    let spotlightSize = 'transparent 160px, black 200px)';
+
+    window.addEventListener('mousemove', e => updateSpotlight(e));
+
+    function updateSpotlight(e) {
+        spotlight.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 100}%, ${spotlightSize}`;
+    }
+});
